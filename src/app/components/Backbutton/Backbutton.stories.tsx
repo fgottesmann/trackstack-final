e5985dfb715c588ab5dbc0fed3a1968457e91b7f
fgottes <f.gottesmann@outlook.com>
2021-06-28 12:08:49 +0200

    ✨ Add back button to storybook

---
+++ src/app/components/Backbutton/Backbutton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Backbutton from './Backbutton';
+
+export default {
+  title: 'Component/Backbutton',
+  component: Backbutton,
+};
+
+export const BackButton = (): JSX.Element => <Backbutton />;
