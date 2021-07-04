324c8d06a204589475e3c7267044cb14fa4bf006
fgottes <f.gottesmann@outlook.com>
2021-07-04 18:12:26 +0200

    ✨ Add video player

---
+++ src/app/components/Video/Video.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import styles from './Video.module.css';
+
+function VideoPlayer(): JSX.Element {
+  return (
+    <div className={styles.videoPlayer}>
+      <ReactPlayer
+        width="23em"
+        height="13em"
+        controls
+        url="/Visualizer-Trackstack.mp4"
+      />
+    </div>
+  );
+}
+
+export default VideoPlayer;
