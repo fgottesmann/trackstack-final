4efd97d492c7112053d740c00082224ac22660fd
fgottes <f.gottesmann@outlook.com>
2021-07-04 20:51:46 +0200

    ✨ Add news segment to homepage

--- src/app/pages/Homepage/Homepage.tsx
+++ src/app/pages/Homepage/Homepage.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './Homepage.module.css';
 import Trackcardhome from '../../components/Trackcard(homepage)/Trackcardhome';
+import VideoPlayer from '../../components/Video/Video';
+import News from '../../components/Tracknews/TrackNews';
 
 export default function Homepage(): JSX.Element {
   return (
-    <div className={styles.trackcardhome}>
-      <Trackcardhome />
+    <div className={styles.homepage}>
+      <a href="http://localhost:3000/tracklist" className={styles.heading}>
+        Stack
+      </a>
+      <div className={styles.trackcardhome}>
+        <Trackcardhome />
+      </div>
+      <div className={styles.news}>
+        <News />
+      </div>
+      <div className={styles.video}>
+        <VideoPlayer />
+      </div>
     </div>
   );
 }
